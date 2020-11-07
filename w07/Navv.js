e9df2dcb4c99a1f6fbb1d40de1ffa144d6233a60
chen zei wei <henry0988170469@gmail.com>
2020-11-07 19:24:10 +0800

    myweb

---
+++ w07/Navv.js
@@ -0,0 +1,30 @@
+function change(index) {
+  //   const p = document.getElementById('show');
+  const p = document.querySelector('#show');
+  console.log('p', p);
+  switch (index) {
+    case 1:
+      p.innerHTML = '<iframe width=100% height=100% src="./w01/intro.html" />';
+      break;
+    case 2:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w01/pokemon.html" />';
+      break;
+    case 3:
+      p.innerHTML = '<iframe width=100% height=100% src="./w02/image.html" />';
+      break;
+    case 4:
+      p.innerHTML = '<iframe width=100% height=100% src="./w02/blog.html" />';
+      break;
+    case 5:
+      p.innerHTML = '<iframe width=100% height=100% src="./w03/div.html" />';
+      break;
+    case 6:
+      p.innerHTML = '<iframe width=100% height=100% src="./w03/div.html" />';
+      break;
+    case 7:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w06/msclone.html" />';
+      break;
+  }
+}
